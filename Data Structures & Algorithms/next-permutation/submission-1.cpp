d014446483cae32cc39579b50033f7c70ce8e8de
Raj Anand <133685133+Raj6571Anand@users.noreply.github.com>
2026-09-25 16:47:31 +0530

    Add: next-permutation - submission-1

---
+++ Data Structures & Algorithms/next-permutation/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int pivot=-1;
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                pivot=i-1;
+                break;
+            }
+        }
+        if(pivot==-1){
+                reverse(nums.begin(), nums.end());
+                
+            }
+            else{
+            int swa=pivot;
+            int diff=INT_MAX;
+            for(int i=pivot;i<nums.size();i++){
+                if(nums[i]>nums[pivot]){
+                    if(diff>nums[i]-nums[pivot]){
+                    diff=nums[i]-nums[pivot];
+                    swa=i;
+                    }
+                    
+                    }
+                    
+                                    
+            }
+            swap(nums[pivot], nums[swa]);
+            reverse(nums.begin() + pivot+1, nums.end());
+            }
+
+            
+
+
+            
+        
+        
+    }
+};
